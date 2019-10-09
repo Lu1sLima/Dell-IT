1c9b53f8a795d65d0735e6cb8ef2ce333d755f53
Luís Lima <llima@cipucrs.pucrs.br>
2019-10-09 17:47:26 -0300

    #6

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -3,7 +3,7 @@ package hello;
 public class Greeter {
     public void sayHello() {
 			for(int i = 0; i < 4; i++){
-				System.out.println("Lucas lixo");
+				System.out.println("Ratiou FEIO!");
 			}
     }
 }
